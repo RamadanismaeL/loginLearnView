9f4df8ddd8f825fba3dc7d0c3a30daf036cf32ce
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-24 21:07:13 +0200

    updated

---
+++ loginViews/src/app/_components/login3/login3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login3',
+  imports: [],
+  templateUrl: './login3.component.html',
+  styleUrl: './login3.component.scss'
+})
+export class Login3Component {
+
+}
--- loginViews/src/app/_pages/home/home.component.ts
+++ loginViews/src/app/_pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Login1Component } from '../../_components/login1/login1.component';
 import { Login2Component } from "../../_components/login2/login2.component";
+import { Login3Component } from "../../_components/login3/login3.component";
 
 @Component({
   selector: 'app-home',
   imports: [
     Login1Component,
-    Login2Component
+    Login2Component,
+    Login3Component
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
