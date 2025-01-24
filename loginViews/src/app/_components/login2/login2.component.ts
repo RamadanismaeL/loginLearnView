28d7731e934e33a0f3151c8916ae03f073456326
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-01-24 15:45:55 +0200

    login2 updated

--- loginViews/src/app/_components/login2/login2.component.ts
+++ loginViews/src/app/_components/login2/login2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login2',
@@ -6,6 +6,36 @@ import { Component } from '@angular/core';
   templateUrl: './login2.component.html',
   styleUrl: './login2.component.scss'
 })
-export class Login2Component {
+export class Login2Component implements AfterViewInit {
+  container: HTMLElement | null = null;
+  register: HTMLElement | null = null;
 
+  ngAfterViewInit(): void {
+    this.container = document.getElementById('container');
+    this.register = document.getElementById('register');
+
+    if (!this.container) {
+      console.error('Container element not found.');
+    }
+
+    if (!this.register) {
+      console.error('Register element not found.');
+    }
+  }
+
+  registerBtn(): void {
+    if (this.container) {
+      this.container.classList.add('active');
+    } else {
+      console.error('Container element not found.');
+    }
+  }
+
+  loginBtn(): void {
+    if (this.container) {
+      this.container.classList.remove('active');
+    } else {
+      console.error('Container element not found.');
+    }
+  }
 }
